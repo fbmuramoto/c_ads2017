d647a486c1a274e5a5e546b8b55df47eef75262d
Liv Muramoto <livmuramoto@gmail.com>
2017-10-24 01:41:11 -0200

    Manipulação de Strings
    Adicionado o conteúdo de Manipulação de Strings.

--- test.c
+++ test.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 
+/*
 //Define tipo lógico	
 
-/*
-Valores lógicos para Verdadeiro (V) e Falso (F):
-• true: valor inteiro 1
-• false: valor inteiro 0
+true: valor inteiro 1
+false: valor inteiro 0
 
-► Código:
------
 enum boolean {
 	true = 1,
 	false = 0
 	};
 
 typedef enum boolean bool; //determina nome da variável do tipo lógico
------
-
-C não tem tipo lógico. Pode ser definido pela declaração a seguir:
-► Código:
------
-#define BOOL char
-#define FALSE 0
-#define TRUE 1
------
 */
 
-int main() {//int substitui void, pois no MacOSX não funciona void...
+int main() { //int substitui void, pois no mac não funciona void...
 
+/*
 //1. Meu primeiro programa em C
 	printf("Meu primeiro programa em C! \n");	
 
@@ -119,6 +108,103 @@ int main() {//int substitui void, pois no MacOSX não funciona void...
 			printf("Operação Inválida \n");
 			break;
 	}
-		 
+
+//9. Laços de repetição
+
+//9.1 Repita-Até-Que
+	char voto, resp;
+	int cont_s, cont_n;
+	
+	cont_s = 0;
+	cont_n = 0;
+	
+	do {
+		printf("Você tem computador em casa? S/N \n");
+		scanf("%c, &voto);
+		fflush(stdin); // não sei o que significa esse código...
+		if (voto == 's') {
+			cont_s++;
+		} else {
+			cont_n++;
+		}
+		printf("Deseja continuar? S/N \n");
+		scanf("%c", &resp);
+		fflush(stdin);
+	} while (resp == 's');
+	printf("Total Sim: %d \n Total Não: %d, cont_s, cont_n);
+
+//9.2 Enquanto-Faça
+	char voto, resp;
+	int cont_s, cont_n;
+	
+	cont_s = 0;
+	cont_n = 0;
+	
+	resp = 's'; //esse item vem no começo no laço enquanto-faça
+	while (resp == 's') {
+		printf("Você tem computador em casa? S/N \n");
+		voto = getch (); // não sei também o que significa esse código...
+		printf("%c \n", voto);
+		if (voto == 's') {
+			cont_s = cont_s + 1;
+		} else {
+			cont_n = cont_n + 1;
+		}
+		printf("Deseja continuar? S/N \n");
+		resp = getch (); // não sei também o que significa esse código...
+		printf("%c \n", resp);
+	}
+	printf("Total Sim: %d \n Total Não: %d, cont_s, cont_n);
+
+//10. Laços Contados
+
+//10.1 Para-Até
+	int fat, i, n;
+	printf("Digite um número inteiro: ");
+	scanf("%d", &n);
+	
+	fat = 1;
+	for (i=1; i<n; i++) {
+		fat = fat*i;
+	}
+	printf("%d \n", fat);
+
+//10.2 Tabuada
+	int numero, i;
+		printf("Escolha um número inteiro: ");
+		scanf("%d", &numero);
+		for (i=1; i<=10; i=i+1) {
+			printf("%2d x %d = %2d \n", i, numero, i*numero);
+		}
+
+//11. Vetores e Matrizes
+	int i;
+	float t[3];
+	float soma, media;
+	
+	soma=0;
+	for (i=0; i<=2; i++) {
+		printf("Digite a temperatura %d: \n", i);
+		scanf("%f", &t[i]);
+		soma=soma+t[i];
+	}
+	media=soma/3;
+	printf("Média das temperaturas: %3.2f \n", media);
+	for (i=0; i<=2; i++) {
+		printf("A diferença %d é %3.2f \n", i, media-t[i]);
+	}
+*/
+
+//12. Manipulação de Strings
+	char c;
+	printf("Digite uma frase: ");
+	do {
+		c = getchar(); //leitura do 'buffer'
+		if (c>=97 && c<=122) { //se c é minúsculo
+			c-=32; //transforma em maiúsculo
+		}
+		putchar(c); //impressão dos caracteres maiúsculos
+	} while (c!='\n');
+
 	return 0; //insere essa linha para retornar um inteiro
 }
